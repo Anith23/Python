e7f6783d450458d5ddad34d5def9032634c803c9
Anitha Anitha <88196992+Anith23@users.noreply.github.com>
2023-10-20 15:59:12 -0400

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,33 @@
+#single line commont
+
+"""
+multi line commont
+"""
+
+a=5
+b = 10
+
+c = a+b
+
+print("a + b = ",c)
+
+A = int(input("Enter the number : "))
+
+print(A)
+
+print(type(A))
+
+
+if A%2 == 0:
+    print("Even")
+else:
+    print("odd")
+
+
+for i in range(1,9):
+
+    print(i*5)
+
+
+furits = ["apple", "bannana"]
+print(furits[0])
